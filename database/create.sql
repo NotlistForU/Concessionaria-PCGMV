fc93b35c383b863ded6c2bdc39fc9e5baff0dbf1
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2026-02-28 09:40:59 -0300

    create database

---
+++ database/create.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS concessionaria;
+USE concessionaria;
+
+CREATE TABLE vehicles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(30) NOT NULL,
+    modelo VARCHAR(30) NOT NULL,
+    motorizacao VARCHAR(30) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    url_foto VARCHAR(255)
+);
+
+CREATE TABLE agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(50) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    data_interesse DATE NOT NULL,
+    veiculo_id INT,
+    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
+);
